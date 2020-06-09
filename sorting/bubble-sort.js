fd4610a41e6f787d217a22783d330f0997a5cd1b
Aiman <aimanaaw@gmail.com>
2020-06-09 02:44:25 -0400

    Optimizing the bubble sort

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,16 +1,33 @@
+// function bubbleSort(arr) {
+// 	for (let i = arr.length; i > 0; i--) {
+// 		for (let j = 0; j < i - 1; j++) {
+			
+// 			if(arr[j] > arr[j+1]) {
+// 				[arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+// 			}
+// 			console.log(arr)
+// 		}
+// 	}
+// 	return arr;
+// }
+
+
 function bubbleSort(arr) {
+	let noSwaps;
 	for (let i = arr.length; i > 0; i--) {
 		for (let j = 0; j < i - 1; j++) {
+			noSwaps = true;
 			
 			if(arr[j] > arr[j+1]) {
-				[arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+				[arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+				noSwaps = false
 			}
-			console.log(arr)
+			if (noSwaps) break;
 		}
 	}
 	return arr;
 }
 
 
-
 console.log(bubbleSort([37, 45, 29, 8]))
+console.log(bubbleSort([8,1,2,3,4,5,6,7]))
