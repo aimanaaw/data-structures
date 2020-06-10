3cc98967f03fbbf4b34f1d24e4781f5522ac484f
Aiman <aimanaaw@gmail.com>
2020-06-09 23:15:51 -0400

    Selection sort

---
+++ sorting/selection-sort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let min = i;
+		let swap;
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[min]) {
+				min = j;
+			}
+			console.log(arr[i], arr[j], arr[min])
+		}
+		if (i !== min) {
+			[arr[i], arr[min]] = [arr[min], arr[i]]
+		}
+	}
+	return arr;
+}
+
+
+console.log(selectionSort([34,22,10,19,17]))
