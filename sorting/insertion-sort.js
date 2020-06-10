72d2111adf4cbb125e3b94b7e316a1cb7e3e6eb2
Aiman <aimanaaw@gmail.com>
2020-06-10 01:36:28 -0400

    Insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,14 @@
+function insertionSort(arr) {
+	for (let i = 1; i < arr.length; i++) {
+		let currentVal = arr[i];
+		for (let j = i - 1; j >= 0; j--) {
+			if(arr[j] > currentVal) {
+				arr[j + 1] = arr[j]
+				arr[j] = currentVal
+			}
+		}
+	}
+	return arr;
+}
+
+console.log(insertionSort([2,1,9,76,4]))
