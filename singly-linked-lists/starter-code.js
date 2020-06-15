102d4e4306bbfda5df95f3faf5024fda3942afd5
Aiman <aimanaaw@gmail.com>
2020-06-14 21:49:57 -0400

    Push method for the singly linked list

--- singly-linked-lists/starter-code.js
+++ singly-linked-lists/starter-code.js
@@ -7,3 +7,32 @@ class Node {
 		this.next = null;
 	}
 }
+
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+	push(val) {
+		const newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+			this.length++;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+			this.length++;
+		}
+		return this;
+	}
+}
+
+let list = new SinglyLinkedList()
+console.log(list.push('hi'));
+console.log(list.push('you'));
+console.log(list.push('99'));
+console.log(list.length);
+console.log(list.head);
+console.log(list.tail);
