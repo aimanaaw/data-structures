d6f7f0cfd31653abbad2e6ed1c45bd6e689b2922
Aiman <aimanaaw@gmail.com>
2020-06-16 14:48:27 -0400

    Push and pop methods

---
+++ doubly-linked-lists/push-method.js
@@ -0,0 +1,44 @@
+// Push a new node at the end
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+			
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+}
+
+let list = new DoublyLinkedList();
+
+list.push('85');
+list.push('87');
+list.push('89');
+list.push('91');
+list.push('93');
+list.push('95');
+list.push('97');
+list.push('99');
+console.log(list);
