cafb0feae3ed252c6efb87d28e4f24de795df328
Aiman <aimanaaw@gmail.com>
2020-06-16 15:03:19 -0400

    Shift method

---
+++ doubly-linked-lists/shift-method.js
@@ -0,0 +1,75 @@
+// Remove a node from the beginning
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	push(val) {
+		let newNode = new Node(val);
+		if (this.length === 0) {
+			this.head = newNode;
+			this.tail = newNode;
+			
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+			this.tail = newNode;
+		}
+		this.length++;
+		return this;
+	}
+
+	pop() {
+		if (this.length === 0) return undefined;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+			return true;
+		} else {
+			let newTail = this.tail.prev;
+			newTail.next = null;
+			this.tail = newTail;
+		}
+		this.length--;
+		return this;
+	}
+
+	shift() {
+		if (this.length === 0) return undefined;
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+			return true;
+		} else {
+			let newHead = this.head.next;
+			newHead.prev = null;
+			this.head = newHead;
+		}
+		this.length--;
+		return this;
+	}
+}
+
+let list = new DoublyLinkedList();
+
+list.push('85');
+list.push('87');
+list.push('89');
+list.push('91');
+list.push('93');
+list.push('95');
+list.push('97');
+list.push('99');
+console.log(list)
+console.log(list.shift());
