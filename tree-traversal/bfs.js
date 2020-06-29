c65a931346bf7bde304c9d530648d0ff3d197725
Aiman <aimanaaw@gmail.com>
2020-06-28 22:51:50 -0400

    bfs

---
+++ tree-traversal/bfs.js
@@ -0,0 +1,76 @@
+// Find method
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(val) {
+		let newNode = new Node(val);
+		if (!this.root) {
+			this.root = newNode;
+			return this;
+		}
+		let current = this.root;
+		let looper = true;
+		while (looper) {
+			if (val > current.value) {
+				if (current.right) {
+					current = current.right;
+				} else {
+					current.right = newNode;
+					looper = false;
+				}
+			} else {
+				if (current.left) {
+					current = current.left;
+				} else {
+					current.left = newNode;
+					looper = false;
+				}
+			}
+		}
+		return this;
+
+	}
+
+	find(val) {
+		if (!this.root) return false;
+		let currentNode = this.root;
+		let found = false;
+		while(currentNode && !found) {
+			if(val < currentNode.value) {
+				currentNode = currentNode.left;
+			} else if (val > currentNode.value) {
+				currentNode = currentNode.right
+			} else {
+				found = true;
+			}
+		}
+		if (!found) return false;
+		return currentNode;
+	}
+
+	// BFS() {
+	// 	let data = [];
+	// 	let queue = [];
+	// 	queue.push(this.root);
+	// }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(6);
+tree.root.left.left = new Node(3);
+tree.root.left.right = new Node(8);
+tree.root.right.right = new Node(20);
+console.log(tree)
