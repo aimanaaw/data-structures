99e1f9468fdcf74708f4af58e1d997c1b237f459
Aiman <aimanaaw@gmail.com>
2020-07-09 01:04:23 -0400

    Adding vertices

--- graphs/vertex.js
+++ graphs/vertex.js
@@ -9,6 +9,12 @@ class Graph {
 		if (!this.adjacencyList[vert]) this.adjacencyList[vert] = [];
 		return this.adjacencyList;
 	}
+
+	addEdge(v1, v2) {
+		this.adjacencyList[v1].push(v2);
+		this.adjacencyList[v2].push(v1);
+		return this.adjacencyList;
+	}
 }
 
 let g = new Graph();
@@ -16,3 +22,4 @@ console.log(g.addVertex("Tokyo"))
 console.log(g.addVertex("Toronto"))
 console.log(g.addVertex("San Francisco"))
 console.log(g.addVertex("Ottawa"))
+console.log(g.addEdge("Tokyo", "Toronto"))
