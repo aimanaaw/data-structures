15acfc46d5760949dc5504e384588eed3e78433d
Aiman <aimanaaw@gmail.com>
2020-07-14 00:47:46 -0400

    Dijkstra's algorithm

--- djikstra/dijkstra-algorithm.js
+++ djikstra/dijkstra-algorithm.js
@@ -35,20 +35,77 @@ class WeightedGraph {
 		return this.adjacencyList;
 	}
 
+	Dijkstra(start, finish) {
+		const nodes = new PriorityQueue();
+		const distances = {};
+		const previous = {};
+		let path = [];
+		let smallest;
+
+		// build up initial state
+		for (let vertex in this.adjacencyList) {
+			if (vertex === start) {
+				distances[vertex] = 0;
+				nodes.enqueue(vertex, 0);
+			} else {
+				distances[vertex] = Infinity
+				nodes.enqueue(vertex, Infinity);
+			}
+			previous[vertex] = null;
+		}
+
+		// As long as there is something to visit
+		while (nodes.values.length) {
+			smallest = nodes.dequeue().val;
+			if (smallest === finish) {
+				while(previous[smallest]) {
+					path.push(smallest);
+					smallest = previous[smallest];
+				}
+				break;
+				// We are done
+				// Build path to return
+			}
+
+			if (smallest || distances[smallest] !== Infinity) {
+				for (let neighbour in this.adjacencyList[smallest]) {
+					let neighbourNode = this.adjacencyList[smallest][neighbour];
+					
+					let candidate = distances[smallest] + neighbourNode.weight;
+
+					let nextNeighbour = neighbourNode.node
+					if (candidate < distances[nextNeighbour]) {
+						// Updating new smallest distance to neighbour
+						distances[nextNeighbour] = candidate;
+						// Updating previous
+						previous[nextNeighbour] = smallest;
+						// Enqueue in the priority queue with new priority
+						nodes.enqueue(nextNeighbour, candidate);
+					}
+				}
+			}
+		}
+		return path.concat(smallest).reverse()
+	}
+
 }
 
 let wg = new WeightedGraph()
 wg.addVertex("A")
 wg.addVertex("B")
 wg.addVertex("C")
+wg.addVertex("D")
+wg.addVertex("E")
+wg.addVertex("F")
 
-console.log(wg.addEdge("A", "B", 9))
-console.log(wg.addEdge("A", "C", 5))
-console.log(wg.addEdge("B", "C", 7))
+wg.addEdge("A", "B", 4)
+wg.addEdge("A", "C", 2)
+wg.addEdge("B", "E", 3)
+wg.addEdge("C", "D", 2)
+wg.addEdge("C", "F", 4)
+wg.addEdge("D", "E", 3)
+wg.addEdge("D", "F", 1)
+wg.addEdge("E", "F", 1)
 
-let q = new PriorityQueue();
-console.log(q.enqueue("B", 3))
-console.log(q.enqueue("C", 5))
-console.log(q.enqueue("D", 2))
-console.log(q.enqueue("Q", 20))
-console.log(q.dequeue())
+console.log(wg.Dijkstra("A", "E"))
+console.log(wg.Dijkstra("A", "C"))
